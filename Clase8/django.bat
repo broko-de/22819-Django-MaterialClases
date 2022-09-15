d2b985fc1734a43471f93cd573552c9b374dddd2
Fedex <jose.liquin@bue.edu.ar>
2022-09-15 11:55:33 -0300

    Se completa el material de la clase 8

---
+++ Clase8/django.bat
@@ -0,0 +1,2 @@
+@echo off
+cmd /k E:\Envs\cac_django_22819\Scripts\activate
---
+++ Clase8/prueba.py
@@ -0,0 +1,13 @@
+import math
+
+from math import pi as piii
+
+from math import *
+
+#math = 34
+
+#print(f'{math*23}')
+
+#print(math.pi)
+#print(pi)
+print(piii)
